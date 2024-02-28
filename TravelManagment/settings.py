96b054fe63067c8ed843cabc1536aff51b1a1663
KandukuriAmar <137085579+KandukuriAmar@users.noreply.github.com>
2024-02-28 18:39:01 +0530

    Update settings.py

--- TravelManagment/settings.py
+++ TravelManagment/settings.py
@@ -128,9 +128,9 @@ STATICFILES_DIRS = [BASE_DIR/"static", ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = '2200030959cseh@gmail.com'
-EMAIL_HOST_PASSWORD = 'jlnhczbvdxxuzdmi'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = '2200030959cseh@gmail.com'
+# EMAIL_HOST_PASSWORD = 'jlnhczbvdxxuzdmi'
